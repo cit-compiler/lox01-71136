f6de3f7f6bcf9ad7700998db156c43eadefb9f38
cike23136 <cike23136@g.nihon-u.ac.jp>
2025-12-02 09:51:09 +0900

    implemented advance(), addToken(), and single-character token recognition (4.6)

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -6,16 +6,60 @@ import java.util.List;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    
+    private int start = 0;     
+    private int current = 0;    
+    private int line = 1;       
 
     public Scanner(String source) {
         this.source = source;
     }
 
     public List<Token> scanTokens() {
-        // ここに字句解析のループ処理が入ります
+        while (!isAtEnd()) {
+            start = current; 
+            scanToken();
+        }
 
-        // 最後にEOFトークンを追加
-        tokens.add(new Token(TokenType.EOF, "", null, 1));
+        tokens.add(new Token(TokenType.EOF, "", null, line));
         return tokens;
+    }
+    
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+    
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private void scanToken() {
+        char c = advance(); 
+
+        switch (c) {
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+            
+            default:
+                System.err.println("Unexpected character: " + c); 
+                break;
+        }
     }
 }
